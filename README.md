83632538176cf07dfa12b2e48ec4324121b63f75
ImCarsen <carsenwarriner@gmail.com>
2023-04-06 23:27:43 -0600

    new file:   README.md     new file:   dotconfig/background.jpg 	new file:   dotconfig/hypr/hyprland.conf 	new file:   dotconfig/hypr/xdg-portal-hyprland 	new file:   dotconfig/swaylock/config 	new file:   dotconfig/waybar/config.jsonc 	new file:   dotconfig/waybar/scripts/waybar-wttr.py 	new file:   dotconfig/waybar/style.css 	new file:   hypr-install.sh

---
+++ README.md
@@ -0,0 +1,49 @@
+<h1 align="center">ImCarsen Hyprland Config</h1>
+<p align="center">
+  <a href="https://choosealicense.com/licenses/mit/">
+    <img alt="License: MIT" src="https://img.shields.io/github/license/ImCarsen/The-Wandering-Cosmos?style=for-the-badge" target="_blank" />
+  </a>
+  <a href="https://github.com/ImCarsen/-/commits/master">
+    <img alt="Last Commit" src="https://img.shields.io/github/last-commit/ImCarsen/-?style=for-the-badge" target="_blank" />
+  </a>
+  <a href="https://github.com/ImCarsen/-/issues">
+    <img alt="Open Issues" src="https://img.shields.io/github/issues/ImCarsen/?style=for-the-badge" target="_blank" />
+  </a>
+</p>
+
+> A personalized version of SolDoesTech and ChrisTitusTech's hyprland configs<br /> 
+
+
+## Screenshots
+
+![Screenshot1]()
+
+
+## Usage
+Made to be run on a fresh install.
+Tested on EndeavourOS Cassini Nova 03-2023 R1 minimal installation
+```
+./hypr-install.sh
+```
+
+## Contributing
+
+Contributions are always welcome!
+Feel free to check [issues page](https://github.com/ImCarsen/-/issues) if you want to contribute.<br />
+
+## Acknowledgements and References
+I'm still super early on in this project and haven't been daily driving Linux for 2+ years. Bear with me.
+
+Original projects sourced from:
+ - [SolDoesTech Hyprland](https://github.com/soldoestech/hyprland)
+ - [ChrisTitusTech Hyprland](https://github.com/ChrisTitusTech/hyprland-titus)
+ 
+## Author
+
+👤 **I'm Carsen**
+
+- Site: [www.imcarsen.com](https://www.imcarsen.com/)
+- Twitter: [@ImCarsen_](https://twitter.com/ImCarsen_)
+- Twitch: [ImCarsen](https://www.twitch.tv/imcarsen)
+- Github: [@ImCarsen](https://github.com/ImCarsen)
+
---
+++ hypr-install.sh
@@ -0,0 +1,91 @@
+#!/bin/bash
+
+# The follwoing will attempt to install all needed packages to run Hyprland
+# This is a quick and dirty script there are no error checking
+# This script is meant to run on a clean fresh system
+# Modified https://github.com/ChrisTitusTech/hyprland-titus and https://github.com/soldoestech/hyprland by https://github.com/ImCarsen
+
+#### Check for yay ####
+ISYAY=/sbin/yay
+if [ -f "$ISYAY" ]; then 
+    echo -e "yay was located, moving on.\n"
+    yay -Suy
+else 
+    echo -e "yay was not located, please install yay. Exiting script.\n"
+    exit 
+fi
+
+### Disable wifi powersave mode ###
+read -n1 -rep 'Would you like to disable wifi powersave? (y,n)' WIFI
+if [[ $WIFI == "Y" || $WIFI == "y" ]]; then
+    LOC="/etc/NetworkManager/conf.d/wifi-powersave.conf"
+    echo -e "The following has been added to $LOC.\n"
+    echo -e "[connection]\nwifi.powersave = 2" | sudo tee -a $LOC
+    echo -e "\n"
+    echo -e "Restarting NetworkManager service...\n"
+    sudo systemctl restart NetworkManager
+    sleep 3
+fi
+
+### Install all packages ####
+#Removed cliphist
+read -n1 -rep 'Would you like to install the packages? (y,n)' INST
+if [[ $INST == "Y" || $INST == "y" ]]; then
+    yay -S --noconfirm hyprland kitty waybar-hyprland   \
+    swww-git swaylock-effects wofi wlogout mako thunar  \
+    thunar-archive-plugin nwg-look-bin                  \
+    ttf-jetbrains-mono-nerd noto-fonts-emoji            \
+    wlr-randr sddm-git starship fish-git                \
+    polkit-kde-agent python-requests                    \
+    swappy grim slurp pamixer brightnessctl gvfs        \
+    xdg-desktop-portal-hyprland-git
+
+    # Start the bluetooth service
+    echo -e "Starting the Bluetooth Service...\n"
+    sudo systemctl enable --now bluetooth.service
+    sleep 2
+    
+    # Clean out other portals
+    echo -e "Cleaning out conflicting xdg portals...\n"
+    yay -R --noconfirm xdg-desktop-portal-gnome xdg-desktop-portal-gtk
+fi
+
+### Copy Config Files ###
+read -n1 -rep 'Would you like to copy config files? (y,n)' CFG
+if [[ $CFG == "Y" || $CFG == "y" ]]; then
+    echo -e "Copying config files...\n"
+    cp ./dotconfig/background.jpg ~/.wallpapers
+    cp -R ./dotconfig/hypr ~/.config/
+    #cp -R ./dotconfig/kitty ~/.config/
+    #cp -R ./dotconfig/pipewire ~/.config/
+    #cp -R ./dotconfig/swaylock ~/.config/
+    cp -R ./dotconfig/waybar ~/.config/
+    #p -R ./dotconfig/wlogout ~/.config/
+    
+    # Set some files as exacutable 
+    chmod +x ~/.config/hypr/xdg-portal-hyprland
+    chmod +x ~/.config/waybar/scripts/waybar-wttr.py
+fi
+
+### Enable SDDM Autologin ###
+read -n1 -rep 'Would you like to enable SDDM autologin? (y,n)' ALOG
+if [[ $ALOG == "Y" || $ALOG == "y" ]]; then
+    LOC="/etc/sddm.conf"
+    echo -e "The following has been added to $LOC.\n"
+    echo -e "[Autologin]\nUser = $(whoami)\nSession=hyprland" | sudo tee -a $LOC
+    echo -e "\n"
+    echo -e "Enable SDDM service...\n"
+    sudo systemctl enable sddm
+    sleep 3
+fi
+
+
+### Script is done ###
+echo -e "Script had completed.\n"
+echo -e "You can start Hyprland by typing Hyprland (note the capital H).\n"
+read -n1 -rep 'Would you like to start Hyprland now? (y,n)' HYP
+if [[ $HYP == "Y" || $HYP == "y" ]]; then
+    exec Hyprland
+else
+    exit
+fi
